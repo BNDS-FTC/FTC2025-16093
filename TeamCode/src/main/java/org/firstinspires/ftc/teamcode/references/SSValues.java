5bc93f396d036f1e89e996fcf38d5171089bf194
Conscious-Pig093 <jianing.yang@hotmail.com>
2024-10-31 20:56:12 +0800

    Intake value fix

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/references/SSValues.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/references/SSValues.java
@@ -32,7 +32,7 @@ public class SSValues {
     public static final double GRAB_CLOSED = 0.55;
 
     public static final double CONTINUOUS_SPIN = 0;
-    public static final double CONTINUOUS_STOP = 0.3;
-    public static final double CONTINUOUS_STOP_OPPOSITE=0.6;
-    public static final double CONTINUOUS_SPIN_OPPOSITE = 0.8;
+    public static final double CONTINUOUS_STOP = 0.25;
+    public static final double CONTINUOUS_STOP_OPPOSITE=0.75;
+    public static final double CONTINUOUS_SPIN_OPPOSITE = 1;
 }
