e78d1ef84ee96deff1e778086e072103ac71b9b6
Conscious-Pig093 <jianing.yang@hotmail.com>
2024-11-12 17:25:11 +0800

    arm changes

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/uppersystems/ArmAction.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/uppersystems/ArmAction.java
@@ -9,8 +9,8 @@ public class ArmAction extends Action {
     //Params not in super class
     private int armTarget;
     private double power = 1;
-    public static double armMinPower = 0.25;
-    public static double armDownCoeffiecient = 1.55;
+    public static double armMinPower = 0.37;
+    public static double armDownCoeffiecient = 1.48;
 
     public ArmAction(SuperStructure upper, int armTarget){
         this.upper = upper;
