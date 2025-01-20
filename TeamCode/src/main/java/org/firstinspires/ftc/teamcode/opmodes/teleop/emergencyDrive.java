1c9dccb684efdf88a24d3e8558de6b03fd79c99a
xiaokai-lyk <105791771+xiaokai-lyk@users.noreply.github.com>
2025-01-20 23:28:35 +0800

    hopelessly, it should be okay...

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/teleop/emergencyDrive.java
@@ -0,0 +1,42 @@
+package org.firstinspires.ftc.teamcode.opmodes.teleop;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+
+@TeleOp(name="OH NO! We are dying!")
+public class emergencyDrive extends LinearOpMode {
+    private DcMotorEx leftFront, leftRear, rightRear, rightFront;
+
+    public void setHeadingPower(double x, double y, double rx) {
+        double botHeading = 0;
+        double rotX = x * Math.cos(-botHeading) - y * Math.sin(-botHeading);
+        double rotY = x * Math.sin(-botHeading) + y * Math.cos(-botHeading);
+
+        rotX = rotX * 1.1;
+
+        double denominator = Math.max(Math.abs(rotY) + Math.abs(rotX) + Math.abs(rx), 1);
+        double frontLeftPower = (rotY + rotX + rx) / denominator;
+        double backLeftPower = (rotY - rotX + rx) / denominator;
+        double frontRightPower = (rotY - rotX - rx) / denominator;
+        double backRightPower = (rotY + rotX - rx) / denominator;
+
+        leftFront.setPower(frontLeftPower);
+        leftRear.setPower(backLeftPower);
+        rightFront.setPower(frontRightPower);
+        rightRear.setPower(backRightPower);
+    }
+
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        waitForStart();
+        leftFront = hardwareMap.get(DcMotorEx.class, "leftFront");
+        leftRear = hardwareMap.get(DcMotorEx.class, "leftBack");
+        rightRear = hardwareMap.get(DcMotorEx.class, "rightBack");
+        rightFront = hardwareMap.get(DcMotorEx.class, "rightFront");
+        while(opModeIsActive()){
+            setHeadingPower(gamepad1.left_stick_x, gamepad1.left_stick_y, gamepad1.right_stick_x);
+        }
+    }
+}
